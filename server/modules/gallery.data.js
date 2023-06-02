350255a1c1fe0cebc9a6fe2dae20b8aed41966b0
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-02 16:57:33 -0500

    added GET and PUT routes for axios, added images to the array with paths to the images folder

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/Sweetroll.jpg', description: 'Let me guess, someone stole your sweetroll?', likes: 0},
+    { id: 3, path: 'images/Bjorn.jpg', description: 'Blood Hunter Bjorn in his native homelands attire', likes: 0},
+    { id: 4, path: 'images/example.jpg', description: 'Yes, very important image', likes: 0},
+    { id: 5, path: 'images/portal-cake.jpg', description: 'The cake is a lie', likes: 0}
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+    let [images, setImages] = useState([]);
+
+    useEffect(() => {
+      getImages()
+    }, [])
+
+    const getImages = () => {
+      axios.get('/gallery')
+        .then(response => {
+          setImages(response.data)
+        })
+        .catch(err => {
+          alert('error getting images');
+          console.log(err);
+        })
+    }
+
+    const likeImage = (id) => {
+      axios.put(`/gallery/like/${id}`)
+      .then(response => {
+        getImages()
+      })
+      .catch(err => {
+        alert('error liking the image')
+        console.log('error with likes', err)
+      })
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {images.map(image => (
+          <div className="image" key={image.id}>
+            <img src={image.path}/>
+        <p><button onClick={likeImage}>👍</button> Likes: {image.likes}</p>
+        </div>
+        ))}
+        
       </div>
     );
 }
