c480727b60ce68b0d8e48c8c7690f7054d71e31a
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-04 16:06:10 -0500

    added code comments to the GalleryItem component

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,14 +1,18 @@
 import { useState , useEffect } from 'react';
 import axios from 'axios';
+// importing the required items for the component to function
 
+// creating a function that will make Items from the props passed to it from GalleryList
 function GalleryItem (props) {
 
+    // creating a conditional to render the description/image
     const [viewPoint, setViewPoint] = useState(true)  
     
+    // setting the props passed along equal to variables for use within the component
     let getImages = props.getImages
-
     let image = props.image
 
+    // creating a function to handle the ability to like an image on the DoM/ update the like counter
     const likeImage = () => {
         let id = image.id
         axios.put(`/gallery/like/${id}`)
@@ -22,6 +26,7 @@ function GalleryItem (props) {
         })
     }
 
+    // creating image items to be sent back and be rendered to the DoM using the props passed form GalleryList
     return (
         <div className="image" key={image.id}>
                 <div onClick={() => setViewPoint(!viewPoint)}>{viewPoint ? <img src={image.path}/> : <p>{image.description}</p>}</div>
@@ -30,4 +35,5 @@ function GalleryItem (props) {
     )
 }
 
+// exporting the created items back to the GalleryList
 export default GalleryItem
