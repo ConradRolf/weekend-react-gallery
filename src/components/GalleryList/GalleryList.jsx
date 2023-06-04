025732c7d7678a4a2c9a704929e9740fd6d495ab
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-04 16:02:17 -0500

    created code comments for the GalleryList component

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,10 +1,16 @@
 import GalleryItem from '../GalleryItem/GalleryItem'
+// importing the galleryitem for rendering
 
+// creating a function to make GalleryItems to render using the props passed from the App
 function GalleryList(props) {
 
+    // setting the imported props equal to variables for use within the component
     let getImages = props.getImages
     let images = props.images
 
+    // things that will be rendered to the DoM/passed back up to the App
+    // also passing along props to the GalleryItem component to create each individual image after looping 
+    // through the array of image objects
     return(
         <div key={images.id}>
         {props.images.map(image => (
@@ -14,4 +20,5 @@ function GalleryList(props) {
     )
 }
 
+// exporting the GalleryList back to the App
 export default GalleryList
